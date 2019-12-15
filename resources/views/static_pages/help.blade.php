24bcdc65be213c896e14391f3ca07bd7f7faef48
mokunshao <mokunshao@foxmail.com>
2019-12-15 19:35:57 +0800

    修改静态页面

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.default')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>coco</title>
-</head>
+@section('title','帮助')
 
-<body>
-  <div>help</div>
-</body>
-
-</html>
+@section('content')
+<div>help</div>
+@endsection
