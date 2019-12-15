af3a648e91db3839ee40c2f562cc6ff295068741
mokunshao <mokunshao@foxmail.com>
2019-12-15 20:24:58 +0800

    修改样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,11 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','coco') - coco</title>
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 
 <body>
-    <div>basic</div>
+    <nav class="navbar">
+        <div class="navbar-left">COCO</div>
+        <div class="navbar-right">
+            <button class="navbar-right-button">帮助</button>
+            <button class="navbar-right-button">登录</button>
+        </div>
+    </nav>
     @yield('content')
+    <script src="{{mix('/js/app.js')}}"></script>
 </body>
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-<div>home</div>
+<div class="home">
+    <div class="home-message">
+        <p>欢迎来到 coco 微博客社区</p>
+        <p>你可以注册或者登录</p>
+    </div>
+</div>
 @endsection
