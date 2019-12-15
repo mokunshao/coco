36fa10910f6981ea33530029d3200de91ed57ba3
mokunshao <mokunshao@foxmail.com>
2019-12-15 22:16:03 +0800

    用户注册页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,6 +6,6 @@
     </div>
     <div class="navbar-right">
         <a href="{{route('help')}}" class="navbar-right-button">帮助</a>
-        <a href="#" class="navbar-right-button">登录</a>
+        <a href="{{route('signup')}}" class="navbar-right-button">登录</a>
     </div>
 </nav>
