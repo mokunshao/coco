61d6eaed64d080520456aca696bdd49251713c46
mokunshao <mokunshao@foxmail.com>
2019-12-16 22:11:08 +0800

    创建会话

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('title','登录')
+
+@section('content')
+@include('shared._errors')
+<div>登录</div>
+<form action="{{route('login')}}" method="post">
+    @csrf
+    <div>
+        <label for="email">邮箱</label>
+        <input id="email" type="text" name="email" value="{{ old('email') }}">
+    </div>
+    <div>
+        <label for="password">密码</label>
+        <input id="password" type="password" name="password" value="{{ old('password') }}">
+    </div>
+    <button type="submit">注册</button>
+</form>
+<div>没有注册？<a href="{{route('signup')}}">现在注册</a></div>
+@endsection
