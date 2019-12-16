656fdb84192b1dd02446197e8c76edca008beafc
mokunshao <mokunshao@foxmail.com>
2019-12-16 22:54:01 +0800

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,7 +5,16 @@
         </a>
     </div>
     <div class="navbar-right">
+        @if (Auth::check())
+        <a href="{{route('users.show',Auth::user())}}">个人中心</a>
+        <form action="{{route('logout')}}" method="post">
+            @method('DELETE')
+            @csrf
+            <button type="submit">退出</button>
+        </form>
+        @else
         <a href="{{route('help')}}" class="navbar-right-button">帮助</a>
         <a href="{{route('signup')}}" class="navbar-right-button">登录</a>
+        @endif
     </div>
 </nav>
