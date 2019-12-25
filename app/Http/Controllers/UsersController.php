4db44a6d3476d4f45373d4272300a456d0ca4d5c
mokunshao <mokunshao@foxmail.com>
2019-12-26 00:24:27 +0800

    注册与登录页面访问限制

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,6 +15,9 @@ class UsersController extends Controller
         $this->middleware('auth', [
             'except' => ['show', 'create', 'store']
         ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
     }
     /**
      * Display a listing of the resource.
