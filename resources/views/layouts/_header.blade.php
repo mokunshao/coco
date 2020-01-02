d2bc9648313decf3b2824572a2a816b33e544d67
mokunshao <mokunshao@foxmail.com>
2020-01-03 01:48:56 +0800

    添加 bulma

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,24 +1,48 @@
-<nav class="navbar">
-    <div class="navbar-left">
-        <a href="{{route('home')}}">
-            COCO
-        </a>
-    </div>
-    <div class="navbar-right">
+<nav>
+    <a href="{{route('home')}}">
+        COCO
+    </a>
+    <div>
         <a href="{{route('users.index')}}">用户列表</a>
         @if (Auth::check())
-        <span>{{Auth::user()->name}}</span>
-        <a href="{{route('users.show',Auth::user())}}">个人中心</a>
+        <a href="{{route('users.show',Auth::user())}}">{{Auth::user()->name}}</a>
         <a href="{{route('users.edit',Auth::user())}}">编辑资料</a>
-        <form action="{{route('logout')}}" method="post">
+        <form action="{{route('logout')}}" method="post" class="inline">
             @method('DELETE')
             @csrf
             <button type="submit">退出</button>
         </form>
         @else
-        <a href="{{route('help')}}" class="navbar-right-button">帮助</a>
-        <a href="{{route('signup')}}" class="navbar-right-button">注册</a>
-        <a href="{{route('login')}}" class="navbar-right-button">登录</a>
+        <a href="{{route('help')}}">帮助</a>
+        <a href="{{route('signup')}}">注册</a>
+        <a href="{{route('login')}}">登录</a>
         @endif
     </div>
+    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        Button
+    </button>
 </nav>
+{{-- <nav class="navbar">
+    <div class="navbar-left">
+        <a href="{{route('home')}}">
+COCO
+</a>
+</div>
+<div class="navbar-right">
+    <a href="{{route('users.index')}}">用户列表</a>
+    @if (Auth::check())
+    <span>{{Auth::user()->name}}</span>
+    <a href="{{route('users.show',Auth::user())}}">个人中心</a>
+    <a href="{{route('users.edit',Auth::user())}}">编辑资料</a>
+    <form action="{{route('logout')}}" method="post">
+        @method('DELETE')
+        @csrf
+        <button type="submit">退出</button>
+    </form>
+    @else
+    <a href="{{route('help')}}" class="navbar-right-button">帮助</a>
+    <a href="{{route('signup')}}" class="navbar-right-button">注册</a>
+    <a href="{{route('login')}}" class="navbar-right-button">登录</a>
+    @endif
+</div>
+</nav> --}}
