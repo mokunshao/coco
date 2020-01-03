0e7122db577baa7b00d48d935a3026c7ae934e06
mokunshao <mokunshao@foxmail.com>
2020-01-04 02:47:38 +0800

    Update create.blade.php

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -5,22 +5,28 @@
 @section('content')
 @include('shared._errors')
 <div class="container">
-    <div>登录</div>
+    <h1 class="title">登录</h1>
     <form action="{{route('login')}}" method="post">
         @csrf
-        <div>
-            <label for="email">邮箱</label>
-            <input id="email" type="text" name="email" value="{{ old('email') }}">
+        <div class="field">
+            <label class="label">邮箱</label>
+            <div class="control">
+                <input class="input" type="text" name="email" value="{{ old('email') }}">
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">密码</label>
+            <div class="control">
+                <input class="input" type="password" name="password">
+            </div>
         </div>
         <div>
-            <label for="password">密码</label>
-            <input id="password" type="password" name="password">
+            <label class="checkbox">
+                <input type="checkbox" name="remember">
+                记住我
+            </label>
         </div>
-        <div>
-            <input type="checkbox" name="remember" id="remember">
-            <label for="remember">记住我</label>
-        </div>
-        <button type="submit">登录</button>
+        <button class="button is-primary" type="submit">登录</button>
     </form>
     <div>没有注册？<a href="{{route('signup')}}">现在注册</a></div>
     <div>忘记密码？<a href="{{route('password.request')}}">现在找回</a></div>
