a83d9ed34567f6f62acafc664d99fdfe88c2b1a8
mokunshao <mokunshao@foxmail.com>
2020-01-03 17:28:47 +0800

    修改样式

--- resources/views/articles/_article.blade.php
+++ resources/views/articles/_article.blade.php
@@ -1,11 +1,37 @@
-<article class="article">
-  <div class="article-avatar-container">
-    <a href="{{ route('users.show', $user->id )}}">
-      <img class="article-avatar" src="{{ $user->gravatar('50') }}" alt="{{ $user->name }}" />
-    </a>
-  </div>
-  <div>
-    <div>{{ $user->name }} <small> / {{ $article->created_at->diffForHumans() }}</small></div>
-    {{ $article->content }}
-  </div>
-</article>
+<div class="box">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-64x64">
+        <img src="{{ $user->gravatar('50') }}" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>{{ $user->name }}</strong> <small>{{ $article->created_at->diffForHumans() }}</small>
+          <br>
+          {{ $article->content }}
+        </p>
+      </div>
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <a class="level-item" aria-label="reply">
+            <span class="icon is-small">
+              <i class="fas fa-reply" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a class="level-item" aria-label="retweet">
+            <span class="icon is-small">
+              <i class="fas fa-retweet" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a class="level-item" aria-label="like">
+            <span class="icon is-small">
+              <i class="fas fa-heart" aria-hidden="true"></i>
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </article>
+</div>
--- resources/views/articles/_article_form.blade.php
+++ resources/views/articles/_article_form.blade.php
@@ -1,10 +1,8 @@
-<div class="article-form">
+<div class="block">
   @include('shared._errors')
   <form action="{{ route('articles.store') }}" method="POST">
     @csrf
-    <textarea rows="3" placeholder="聊聊新鲜事儿..." name="content" required></textarea>
-    <div>
-      <button type="submit">发布</button>
-    </div>
+    <textarea class="textarea" placeholder="聊聊新鲜事儿..." name="content" required></textarea>
+    <button class="button is-info" type="submit">发布</button>
   </form>
 </div>
