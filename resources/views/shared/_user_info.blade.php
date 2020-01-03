702457e25094e9980fabc05e4e2f536ed3728abe
mokunshao <mokunshao@foxmail.com>
2020-01-03 23:43:43 +0800

    Update _user_info.blade.php

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -5,4 +5,9 @@
     <a href="{{ route('users.show', $user->id) }}">
         <div>{{$user->name}}</div>
     </a>
+    <div>
+        <div>关注：？</div>
+        <div>被关注：？</div>
+        <div>微博数：？</div>
+    </div>
 </div>
