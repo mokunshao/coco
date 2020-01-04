1e6822048f3c5ecb93a427f5cfdfc49bf1889207
mokunshao <mokunshao@foxmail.com>
2020-01-04 14:56:04 +0800

    修改样式

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,32 +4,46 @@
 
 @section('content')
 @include('shared._errors')
-<div>更新个人资料</div>
-<div>
-    <a href="http://gravatar.com/emails" target="_blank">
-        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" />
-    </a>
-</div>
-<form action="{{route('users.update',$user)}}" method="post">
-    @method('PATCH')
-    @csrf
-    <div>
-        <label for="name">名称</label>
-        <input type="text" name="name" value="{{ $user->name }}">
+<div class="container">
+    <h1 class="title">更新个人资料</h1>
+    <div class="block">
+        <a href="http://gravatar.com/emails" target="_blank">
+            <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" />
+        </a>
     </div>
-    <div>
-        <label for="email">邮箱</label>
-        <input disabled type="email" name="email" value="{{ $user->email }}">
-    </div>
-    <div>
-        <label for="password">密码</label>
-        <input type="password" name="password" value="{{ old('password') }}">
-    </div>
+    <form action="{{route('users.update',$user)}}" method="post">
+        @method('PATCH')
+        @csrf
 
-    <div>
-        <label for="password_confirmation">确认密码</label>
-        <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}">
-    </div>
-    <button type="submit">更新</button>
-</form>
+        <div class="field">
+            <label class="label">名称</label>
+            <div class="control">
+                <input class="input" type="text" name="name" value="{{ $user->name }}">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">邮箱</label>
+            <div class="control">
+                <input class="input" disabled readonly type="email" name="email" value="{{ $user->email }}">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">密码</label>
+            <div class="control">
+                <input class="input" type="password" name="password">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">确认密码</label>
+            <div class="control">
+                <input class="input" type="password" name="password_confirmation">
+            </div>
+        </div>
+
+        <button class="button is-primary" type="submit">更新</button>
+    </form>
+</div>
 @endsection
