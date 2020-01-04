125cb44cef0299966c88781298abd765ccb216a5
mokunshao <mokunshao@foxmail.com>
2020-01-04 14:23:25 +0800

    修改样式

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,26 +2,25 @@
 @section('title','重置密码')
 
 @section('content')
-<h1>重置密码</h1>
+@include('shared._errors')
 
 @if (session('status'))
-<div class="alert alert-success">
+<div class="notification is-success">
   {{ session('status') }}
 </div>
 @endif
 
-@if ($errors->has('email'))
-<span class="form-text">
-  <strong>{{ $errors->first('email') }}</strong>
-</span>
-@endif
-
-<form action="{{route('password.email')}}" method="post">
-  @csrf
-  <div>
-    <label for="email">邮箱</label>
-    <input id="email" type="email" name="email">
-  </div>
-  <div><button type="submit">发送密码重置邮件</button></div>
-</form>
+<div class="container">
+  <h1 class="title">重置密码</h1>
+  <form action="{{route('password.email')}}" method="post">
+    @csrf
+    <div class="field">
+      <label class="label">邮箱</label>
+      <div class="control">
+        <input class="input" type="text" name="email">
+      </div>
+    </div>
+    <div><button class="button is-primary" type="submit">发送密码重置邮件</button></div>
+  </form>
+</div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,14 +2,17 @@
 @section('title', $user->name)
 
 @section('content')
-@include('shared._user_info',['user'=>$user])
+<div class="container">
+  @include('shared._user_info',['user'=>$user])
 
-@if ($articles->count()>0)
-@foreach ($articles as $article)
-@include('articles._article')
-@endforeach
-@else
-<p>没有数据</p>
-@endif
+  @if ($articles->count()>0)
+  @foreach ($articles as $article)
+  @include('articles._article')
+  @endforeach
+  {{$articles->links()}}
+  @else
+  <p>没有数据</p>
+  @endif
+</div>
 
 @endsection
