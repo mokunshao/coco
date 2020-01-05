6f1507d071ca957f8b5b62859deaa968e57671b4
mokunshao <mokunshao@foxmail.com>
2020-01-06 01:22:21 +0800

    Update _article.blade.php

--- resources/views/articles/_article.blade.php
+++ resources/views/articles/_article.blade.php
@@ -17,14 +17,19 @@
           <br>
           {{ $article->content }}
           <br>
-          @can('destroy', $article)
-          <form action="{{route('articles.destroy',$article)}}" method="post"
-            onsubmit="return confirm('您确定要删除本条微博吗？');">
-            @csrf
-            @method('DELETE')
-            <button class="button is-small is-danger is-outlined">删除</button>
-          </form>
-          @endcan
+          <div class="is-flex is-marginless">
+            <a href="{{route('articles.show',$article)}}">
+              <button class="button is-small">详情</button>
+            </a>
+            @can('destroy', $article)
+            <form action="{{route('articles.destroy',$article)}}" method="post"
+              onsubmit="return confirm('您确定要删除本条微博吗？');">
+              @csrf
+              @method('DELETE')
+              <button class="button is-small is-danger is-outlined">删除</button>
+            </form>
+            @endcan
+          </div>
         </p>
       </div>
     </div>
